02a74fbe4b115f591a0f68f67e882f7771d0ae2e
Juan-karlos <juank21mal@gmail.com>
2023-08-14 16:00:14 -0600

    login terminado

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const morgan = require('morgan')
+const passport = require('passport')
 const app = express()
 
 app.set('port',process.env.PORT ||3200)
@@ -7,7 +8,12 @@ app.set('port',process.env.PORT ||3200)
 app.use(express.json()),
 app.use(morgan('dev')),
 app.use(express.urlencoded({extended:false}))
+app.use(passport.initialize());
 
-app.use(require("./routes/verificacion.routes"))
+
+app.use('/api/login',require("./routes/login.routes"))
+app.use('/api/login',require("./routes/reg_requeriminto.routes"))
+
+
 
 module.exports=app
