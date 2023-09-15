e9a6c2545e3de51e960e1c9c0165de2ff9a501f3
Juan-karlos <juank21mal@gmail.com>
2023-09-15 11:39:26 -0600

    Cambios en las tutas al requerir el id para hacer actividades

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ require ('dotenv').config({path:'./src/.env'})
 PORT = process.env.PORT
 DB_HOST = process.env.DB_HOST 
 DB_USER=process.env.DB_USER
-DB_PASSWORD = process.env.DB_PASSWORD 
+DB_PASSWORD = process.env.DB_PASSWORD
 DB_NAME =process.env.DB_NAME
 DB_PORT=process.env.DB_PORT
  
