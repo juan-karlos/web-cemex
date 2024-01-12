690f30bf11182f0d267f624ee30c7da37c04b692
Fortijc <fortijc@gmail.com>
2024-01-11 18:53:42 -0600

    Auto stash before rebase of "origin/Forty"
    manejo de errores en validación de usuarios.

--- src/controllers/valUsuario.js
+++ src/controllers/valUsuario.js
@@ -9,8 +9,12 @@ const { token } = require('morgan');
 const controladorUsuario={}
 
 controladorUsuario.uniUsuario = async(req,res )=>{
-    const [usuario] = await pool.query("select *  From usuarios ");
-    res.send(usuario)
+    try {
+        const [usuario] = await pool.query("select *  From usuarios ");
+        res.send(usuario)
+    } catch (error) {
+        res.status(500).json('No fue posible establecer conexion con el servidor.')
+    }
 }
 
 controladorUsuario.regisUsu=async(req,res)=>{
@@ -83,105 +87,137 @@ controladorUsuario.regisUsu=async(req,res)=>{
         });
 
         res.status(200).json({ message: "Se ha enviado un correo para verificar el registro con tiempo limite de 15 minutos" });
-        console.log(tokenData)
     } catch (error) {
-        res.json({
-            message: 'Verica que hayas insertado los datos adecuadamente'
+        res.status(500).json({
+            message: 'No fue posible establecer conexion con el servidor.'
         })
     }      
 }    
 
 controladorUsuario.verificaRegistro = async (req, res) => {
-    const tokenData = req.params.tokenData;
     try {
-        const decodedToken = jwt.verify(tokenData, 'secreto');
-        const { correo, user, apellidos, passwordHash } = decodedToken;
-        
-        const [email] = await pool.query('SELECT correo_electronico FROM usuarios WHERE correo_electronico = ?',[correo])
-        if(email.length <= 0){
-            // Realiza la inserción del usuario en la base de datos
-            const [insert] = await pool.query('INSERT INTO usuarios (correo_electronico, nombre_usuario, apellidos, contrasena) VALUES (?, ?, ?, ?)', [correo, user, apellidos, passwordHash]);
-            res.status(200).json({ message: "Registro completado con éxito" });
-        }else{
-            res.json({error: 'El correo que ingresaste ya existe'});
-        }
+        const tokenData = req.params.tokenData;
+        try {
+            const decodedToken = jwt.verify(tokenData, 'secreto');
+            const { correo, user, apellidos, passwordHash } = decodedToken;
+            
+            const [email] = await pool.query('SELECT correo_electronico FROM usuarios WHERE correo_electronico = ?',[correo])
+            if(email.length <= 0){
+                // Realiza la inserción del usuario en la base de datos
+                const [insert] = await pool.query('INSERT INTO usuarios (correo_electronico, nombre_usuario, apellidos, contrasena) VALUES (?, ?, ?, ?)', [correo, user, apellidos, passwordHash]);
+                res.status(200).json({ message: "Registro completado con éxito" });
+            }else{
+                res.json({error: 'El correo que ingresaste ya existe'});
+            }
 
-    } catch (excepcion) {
-        if (excepcion.name === 'TokenExpiredError') {
-            res.status(401).json({ error: 'Token expirado'});
+        } catch (excepcion) {
+            if (excepcion.name === 'TokenExpiredError') {
+                res.status(401).json({ error: 'Token expirado'});
+            }
         }
+    } catch (error) {
+        res.status(500).json('No fue posible conectar con el servidor.')
     }
 };
 
 controladorUsuario.cancelacion = async (req,res)=>{
-    const tokenv = req.params.token;
-    console.log(tokenv)
-    res.status(401).json({
-        message: 'No ha sido validado el registro', tokenv
-    })
+    try {
+        const tokenv = req.params.token;
+
+        res.status(401).json({
+            message: 'No ha sido validado el registro', tokenv
+        })
+    } catch (error) {
+        res.status(500).json('No fue posible conectar con el servidor.')
+    }
 }
 
-controladorUsuario.comparacion= async(req,res)=>{
-    const correo= req.body.correo;
-    const contra = req.body.password;
+controladorUsuario.comparacion= async(req,res)=>{ ///----------revisar
+    try {
+        const correo= req.body.correo;
+        const contra = req.body.password;
 
-    const [bdpassword] =  await pool.query('select contrasena from usuarios where correo_electronico= ? ',[correo]); 
-    
-    const encriptedbd = bdpassword[0]['contrasena']
-    let compare =bcryptjs.compareSync(contra,encriptedbd);
-    if(compare){
-        await transporter.sendMail({
-            from: '"Admin" <fortijc@gmail.com>', // sender address
-            to: correo, // list of receivers
-            subject: 'Forgot password', // Subject line
-            html: `
-            <b>Please click on the following link, or paste this into your browser to complete the process</b>
-            `,
-          }); 
-        res.json("Bienvenido");
-    }else{
-        res.json("no se encuentra el usuario");
+        const [bdpassword] =  await pool.query('select contrasena from usuarios where correo_electronico= ? ',[correo]); 
+        if (bdpassword.length>0) {
+            const encriptedbd = bdpassword[0]['contrasena']
+            let compare =bcryptjs.compareSync(contra,encriptedbd);
+            if(compare){
+                res.json("Bienvenido");
+            }else{
+                res.json("no se encuentra el usuario");
+            }
+        } else {
+            res.status(400).json('Verifica que este escrito bien tu correo.')
+        }
+
+    } catch (error) {
+        res.status(500).json('No se pudo establecer conexion con el servidor.')
     }
     
 }
 
-controladorUsuario.eliminar =async(req,res)=>{
-    const correo = req.body.correo;
+controladorUsuario.eliminar =async(req,res)=>{////---revisar
+    try {
+        const correo = req.body.correo;
+        const contrasena = req.body.password;
 
-    const [bdpassword] =  await pool.query('select contrasena from usuarios where correo_electronico= ? ',[correo]); 
-    contrabd = JSON.stringify(bdpassword);
-    let encriptedbd = contrabd.substring(16,76);
+        const [bdpassword] =  await pool.query('select contrasena from usuarios where correo_electronico= ?',[correo]); 
 
-    const contrasena = req.body.password;
+        if (bdpassword.length>0) {
+            contrabd = JSON.stringify(bdpassword);
+            let encriptedbd = contrabd.substring(16,76);
 
-    let compare =bcryptjs.compareSync(contrasena,encriptedbd);
-    if(compare){
-        await pool.query('Delete From usuarios Where correo_electronico= ? ',[correo]);
-        res.send("Usuario eliminado");
-    }else{
-        res.send("contraseña o correo electronico no es correcto");
-    } 
+            let compare =bcryptjs.compareSync(contrasena,encriptedbd);
+            
+            if(compare){
+                await pool.query('Delete From usuarios Where correo_electronico= ? ',[correo]);
+                res.status(200).json("Usuario eliminado.");
+            }else{
+                res.status(400).json("contraseña o correo electronico no es correcto.");
+            } 
+        } else {
+            res.status(400).json('No se encontro ningun correo que cincida con el que proporcionaste.')
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            Error: 'No se pudo conectar con el servidor.'
+        })
+    }
 }
 
 controladorUsuario.actualizarContrasena=async(req,res)=>{
+    try {
+        const correo= req.body.correo;
+        const contra = req.body.password;
+        const contranueva= req.body.passnuevo
+        
+        const [bdpassword] =  await pool.query('select contrasena from usuarios where correo_electronico= ? ',[correo]); 
+        if (bdpassword.length>0) {
+            // const passwor = bdpassword[0].params.contrasena;
+            contrabd = JSON.stringify(bdpassword);
+            let encriptedbd = contrabd.substring(16,76);
+            let compare =bcryptjs.compareSync(contra,encriptedbd);
+            if(compare){
+                let passwordHash= await bcryptjs.hash(contranueva,8)
+                await pool.query(`UPDATE usuarios SET contrasena=ifNULL(?,contrasena) WHERE correo_electronico=?`,[passwordHash,correo]);
+                res.json("se actualizon con exito");
+            }else{
 
-    const correo= req.body.correo;
-    const contranueva= req.body.passnuevo
-    
-    const [bdpassword] =  await pool.query('select contrasena from usuarios where correo_electronico= ? ',[correo]); 
-    const contra = req.body.password;
-    // const passwor = bdpassword[0].params.contrasena;
-    contrabd = JSON.stringify(bdpassword);
-    let encriptedbd = contrabd.substring(16,76);
-    let compare =bcryptjs.compareSync(contra,encriptedbd);
-    if(compare){
-        let passwordHash= await bcryptjs.hash(contranueva,8)
-        await pool.query(`UPDATE usuarios SET contrasena=ifNULL(?,contrasena) WHERE correo_electronico=?`,[passwordHash,correo]);
-        res.json("se actualizon con exito");
-    }else{
-
-        res.json("no se encuentra el usuario");
+                res.json("no se encuentra el usuario");
+            }
+        } else {
+            res.status(400).json({
+                message: 'No se encontro una contraseña ligada al correo que proporcionaste'
+            })
+        }
+        
+    } catch (error) {
+        res.status(500).json({
+            message: 'No hay conexion con el servidor'
+        })
     }
+
  }
 
  module.exports=controladorUsuario
