b912350572f353ff74a746e79ec06d2b79dad6f3
Juan-karlos <juank21mal@gmail.com>
2024-03-15 12:00:04 -0600

    Modificación de los usuarios
    Se hicieron nuevos métodos para la verificación de usuarios y de roles

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -7,7 +7,9 @@ router.get("/verifica/:token/:tokenData", valUsuario.verificaRegistro);
 router.get("/cancelar/:token", valUsuario.cancelacion);
 router.post("/comparacion", valUsuario.comparacion);
 router.get("/", valUsuario.uniUsuario);
-
+router.get("/usuarios",valUsuario.todosUsuarios)
 router.delete("/eliminar", valUsuario.eliminar);
 router.patch("/actualizarContra", valUsuario.actualizarContrasena);
+
+router.post("/administrador",valUsuario.administrador)
 module.exports = router;
